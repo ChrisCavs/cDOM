79f04807dd53df2bdf154a9cafa04dedf8b08421
Chris <chris211321@gmail.com>
2018-07-23 16:21:10 -0400

    ajax returning promise

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './lib/main.js',
   output: {
-    path: path.resolve(__dirname, 'lib'),
+    path: path.resolve(__dirname),
     filename: 'bundle.js'
   },
   devtool: 'source-map'
